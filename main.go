53e6e2b099615dbf7960712af8efd78cdb825457
Nicholas <itsnichkrueger@gmail.com>
2024-05-13 09:27:28 -0500

    add routes

--- main.go
+++ main.go
@@ -7,14 +7,34 @@ import (
 )
 
 func main() {
-	// Create a Gin router
 	router := gin.Default()
 
-	// Define a route handler
+	router.Static("/static", "./static")
+
+	// Define a route handler for the root path
 	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "Hello, You Created a Web App!"})
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, You created a Web App!"})
 	})
 
-	// Run the server on port 8080
+	// Define a route handler for "/about"
+	router.GET("/about", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "About Us"})
+	})
+
+	// Define a route handler for "/contact"
+	router.GET("/contact", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Contact Us"})
+	})
+
+	router.GET("/hello/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, " + name + "!"})
+	})
+
+	router.GET("/profile/:username", func(c *gin.Context) {
+		username := c.Param("username")
+		c.HTML(http.StatusOK, "profile.html", gin.H{"username": username})
+	})
+
 	router.Run(":8080")
 }
